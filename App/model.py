6190d60a8a27c0fc894f709f9514b2ffdacc20a3
José Nicolás Torres Ortiz <69617274+ansternew@users.noreply.github.com>
2021-03-03 13:20:21 -0500

    Update model.py

--- App/model.py
+++ App/model.py
@@ -1,4 +1,4 @@
-﻿"""
+"""
  * Copyright 2020, Departamento de sistemas y Computación,
  * Universidad de Los Andes
  *
@@ -29,7 +29,13 @@ import config as cf
 from DISClib.ADT import list as lt
 from DISClib.Algorithms.Sorting import shellsort as sa
 assert cf
+from DISClib.Algorithms.Sorting import insertionsort as ints
+from DISClib.Algorithms.Sorting import selectionsort as sets
+from DISClib.Algorithms.Sorting import shellsort as shls
+from DISClib.Algorithms.Sorting import quicksort as qcks
+from DISClib.Algorithms.Sorting import mergesort as mrgs
 
+import time
 """
 Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
 los mismos.
@@ -37,7 +43,7 @@ los mismos.
 
 # Construccion de modelos
 
-def newCatalog():
+def newCatalog(tipo):
     """
     Inicializa el catálogo de videos. Crea una lista vacia para guardar
     todos los videos, adicionalmente, crea una lista vacia para las categorias. 
@@ -46,8 +52,8 @@ def newCatalog():
     catalog = {'videos': None,
                'categorias': None,}
 
-    catalog["videos"] = lt.newList()
-    catalog["categorias"] = lt.newList()
+    catalog["videos"] = lt.newList(datastructure=tipo)
+    catalog["categorias"] = lt.newList(datastructure=tipo)
 
     return catalog
 
@@ -80,4 +86,38 @@ def newCategoria(name, id):
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
+def cmpVideosbyViews(video1,video2):
+    return ((video1["views"]) < (video2["views"]))
 # Funciones de ordenamiento
+
+def sortVideos(catalog, size, tipo):
+    sub_list = lt.subList(catalog["videos"], 1, tipo)
+    sub_list = sub_list.copy()
+
+    if tipo == "Insertion":
+        start_time = time.process_time()
+        sorted_list = ints.sort(sub_list, cmpVideosbyViews)
+        stop_time = time.process_time()
+
+    elif tipo == "Selection":
+        start_time = time.process_time()
+        sorted_list = sets.sort(sub_list, cmpVideosbyViews)
+        stop_time = time.process_time()
+
+    elif tipo == "Shell":
+        start_time = time.process_time()
+        sorted_list = shls.sort(sub_list, cmpVideosbyViews)
+        stop_time = time.process_time()
+    
+    elif tipo == "Merge":
+        start_time = time.process_time()
+        sorted_list = mrgs.sort(sub_list, cmpVideosbyViews)
+        stop_time = time.process_time()
+
+    elif tipo == "Quick":
+        start_time = time.process_time()
+        sorted_list = qcks.sort(sub_list, cmpVideosbyViews)
+        stop_time = time.process_time()
+
+    Tiempo_total = (stop_time-start_time)*1000
+    return Tiempo_total, sorted_list
