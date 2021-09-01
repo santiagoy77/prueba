c542a2b2622644cb19b308cb9e8dd3ff04e453ce
an-sanabria <sanabria.an1312@gmail.com>
2021-09-01 11:04:41 -0500

    Adelanto básico del Reto

--- App/model.py
+++ App/model.py
@@ -37,6 +37,42 @@ los mismos.
 
 # Construccion de modelos
 
+def newCatalog():
+  """
+  """
+  artists_catalog = {'ConstituentID': None,
+                     'DisplayName': None,
+                     'ArtistBio': None,
+                     'Nationality': None,
+                     'Gender': None,
+                     'BeginDate': None,
+                     'EndDate': None,
+                     'Wiki QID': None,
+                     'ULAN': None}
+  artworks_catalog = {'ObjectID': None,
+                      'Title': None,
+                      'ConstituentID': None,
+                      'Date': None,
+                      'Medium': None,
+                      'Dimensions': None,
+                      'CreditLine': None,
+                      'AccessionNumber': None,
+                      'Classification': None,
+                      'Department': None,
+                      'DateAcquired': None,
+                      'Cataloged': None,
+                      'URL': None,
+                      'Circumference (cm)': None,
+                      'Depth (cm)': None,
+                      'Diameter (cm)': None,
+                      'Height (cm)': None,
+                      'Lenght (cm)': None,
+                      'Weight (kg)': None,
+                      'Width (cm)': None,
+                      'Seat Height (cm)': None,
+                      'Duration (sec.)': None}
+  artworks_catalog['ConstituentID'] = lt.newList()
+
 # Funciones para agregar informacion al catalogo
 
 # Funciones para creacion de datos
--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-#ISIS1225 - Librerias de soporte
+# Reto #1
 
-Este proyecto contiene los Tipos abstractos de datos, estructuras de datos y algoritmos requeridos por el curso ISIS1225-Estructuras de Datos y Algoritmos
+## Miembros del grupo
 
--Lib
-Este directorio contiene todo el código de base que se entrega para el funcionamiento del curso.  Dentro de este directorio encuentran:
-    
-    |-- ADT:  Directorio con los Tipos Abstractos de Datos del curso
-
-    |--DataStructures: Directorio con todas las estructuras de datos y archivos auxiliares para su     correcto funcionamiento
-
-    |--Algorithms: Implementación de los algoritmos vistos en el curso (por ejemplo algoritmos de ordenamiento)
-
-    |-- Utils: Funciones auxiliares para el funcionamiento de los TADs y estructuras de datos
-
--Test
-Este directorio contiene las pruebas realizadas a los TADs, Estructuras de Datos y Algoritmos.
-
+* Req. 3 - Andrés Sanabria, 202113000, ja.sanabriag1@uniandes.edu.co
+* Req. 4 - Juan Daniel Sepúlveda, 202113067, j.sepulvedao@uniandes.edu.co
