a44f515a58737c2190b39fc08d385a81917d4d70
alejo2612 <78027798+alejo2612@users.noreply.github.com>
2021-09-01 21:36:37 -0500

    Menú

--- App/view.py
+++ App/view.py
@@ -37,7 +37,13 @@ operación solicitada
 def printMenu():
     print("Bienvenido")
     print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("2- Listar cronológicamente los artistas ")
+    print("3- Listar cronológicamente las adquisiciones")
+    print("4- Clasificar las obras de un artista por técnica")
+    print("5- Clasificar las obras por la nacionalidad de sus creadores")
+    print("6- Transportar obras de un departamento ")
+    print("7- Proponer una nueva exposición en el museo ")
+    print("0- Salir"
 
 catalog = None
 
@@ -49,6 +55,10 @@ while True:
     inputs = input('Seleccione una opción para continuar\n')
     if int(inputs[0]) == 1:
         print("Cargando información de los archivos ....")
+        catalog = initCatalog()
+        loadData(catalog)
+        print('Artistas cargados: ' + str(lt.size(catalog['artists'])))
+        print('Obras cargadas: ' + str(lt.size(catalog['artworks'])))
 
     elif int(inputs[0]) == 2:
         pass
--- Data/readme.txt
+++ Data/readme.txt
@@ -1,1 +1,4 @@
 Utilizar los archivos de datos que se encuentran en Sicuaplus
+
+Requerimiento 3 - Hugo David Flórez Rodríguez, 201912429, hd.florez@uniandes.edu.co
+Requerimiento 4 - Alejandro Herrera Jiménez, 201915788, a.herrera20@uniandes.edu.co
