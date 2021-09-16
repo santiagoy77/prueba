e25468b2da86ba164dac776c7099fded4477c8e7
Federico Melo Barrero <federicomelobarrero@gmail.com>
2021-09-15 22:10:10 -0500

    Avance Req 1 ordenamientos - Reto 1

--- App/view.py
+++ App/view.py
@@ -28,7 +28,7 @@ assert cf
 
 """
 La vista se encarga de la interacción con el usuario
-Presenta el menu de opciones y por cada seleccion
+Presenta el menú de opciones y por cada seleccion
 se hace la solicitud al controlador para ejecutar la
 operación solicitada
 """
