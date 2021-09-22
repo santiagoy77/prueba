2580309de70e22ef04c923c52fb34a148bf19f2d
Federico Melo Barrero <federicomelobarrero@gmail.com>
2021-09-21 19:27:00 -0500

    Tablas

--- App/view.py
+++ App/view.py
@@ -24,6 +24,7 @@ import config as cf
 import sys
 import controller
 from DISClib.ADT import list as lt
+from prettytable import PrettyTable
 assert cf
 
 """
@@ -37,7 +38,7 @@ def printMenu():
     """
     Imprime las opciones del menú.
     """
-    print("Menú de opciones:\n")
+    print("\nMenú de opciones:\n")
     print("1- Cargar información en el catálogo.")
     print("2- Listar cronológicamente los artistas.")
     print("3- Listar cronológicamente las adquisiciones.")
@@ -80,8 +81,10 @@ while True:
         artwork=controller.sortArtworks_DateAcquired(catalog)
         catalog["artists_BeginDate"]=artist[1]
         catalog["artworks_DateAquired"]=artwork[1]
-        print('Número de artistas en el catálogo: ' + str(lt.size(catalog['artists_BeginDate'])))
-        print('Número de obras de arte en el catálogo: ' + str(lt.size(catalog['artworks_DateAquired'])))
+        print('Número de artistas en el catálogo: ',
+              str(lt.size(catalog['artists_BeginDate'])))
+        print('Número de obras de arte en el catálogo: ',
+              str(lt.size(catalog['artworks_DateAquired'])))
         print("Se demoro: ",str(artist[0]))
         print('Últimos tres artistas cargados:\n')
         for i in [-2,-1,0]:
@@ -96,30 +99,46 @@ while True:
         anio2=int(input("Digite un año final: "))
         result=controller.rangoArtists(catalog, anio1, anio2)
         print("======================== Req No. 1 Inputs ========================")
-        print("Artistas nacidos entre ",str(anio1)," y ",str(anio2))
+        print("Artistas nacidos entre ",str(anio1)," y ",str(anio2)),"."
         print("======================== Req No. 1 Respuesta ========================")
-        print("Hay ",str(lt.size(result))," artistas nacidos entre ",str(anio1)," y ",str(anio2))
-        print('\nTres artistas con menor fecha de nacimiento: :\n')
-        for i in [1,2,3]:
-            print(str(lt.getElement(result,i)))
-        print('\nTres artistas con mayor fecha de nacimiento: \n')
-        for i in [-2,-1,0]:
-            print(str(lt.getElement(result,i)))
+        print("Hay ",str(lt.size(result))," artistas nacidos entre ",
+              str(anio1)," y ",str(anio2)),"."
+        print('\nPrimeros y últimos tres artistas nacidos en el rango de años:\n')
+        answ = PrettyTable(['Nombre','Nacimiento','Fallecimiento'
+                            ,'Nacionalidad','Género'])
+        for i in [1,2,3,-2,-1,0]:        
+            answ.add_row([lt.getElement(result,i)['DisplayName'],
+                          lt.getElement(result,i)['BeginDate'],
+                          lt.getElement(result,i)['EndDate'],
+                          lt.getElement(result,i)['Nationality'],
+                          lt.getElement(result,i)['Gender']])
+        answ._max_width = {'Nombre':40}
+        print(answ)
 
     elif inputs==3:
         fecha1=input("Ingrese una fecha inicial en formato AAAA-MM-DD: ")
         fecha2=input("Ingrese una fecha final en formato AAAA-MM-DD: ")
         result = controller.rangoArtworks(catalog, fecha1, fecha2)
+        num_artists = 5 # TODO: FALTA
+        num_purchased = 5 # TODO: Falta
         print("======================== Req No. 1 Inputs ========================")
-        print("Artistas nacidos entre ",fecha1," y ",fecha2)
+        print("Obras adquiridas entre ",fecha1," y ",fecha2)
         print("======================== Req No. 1 Respuesta ========================")
-        print("Hay ",str(lt.size(result))," artistas nacidos entre ",fecha1," y ",fecha2)
-        print('\nPrimeras tres obras de arte adquiridas:\n')
-        for i in [1,2,3]:
-            print(str(lt.getElement(result,i)))
-        print('\nÚltimas tres obras de arte adquiridas:\n')
-        for i in [-3,-2,-1]:
-            print(str(lt.getElement(result,i)))
+        print("El MoMA adquirió ",str(lt.size(result))," obras entre ",fecha1,
+              " y ",fecha2," con ",str(num_artists),
+              " artistas, de las cuales compró ",str(num_purchased))
+        print('\nPrimeras y últimas tres obras adquiridas en el rango de fechas:\n')
+        answ = PrettyTable(['Título','Artista(s)','Fecha','Medio',
+                            'Dimensiones'])
+        for i in [1,2,3,-2,-1,0]:        
+            answ.add_row([lt.getElement(result,i)['Title'],
+                          lt.getElement(result,i)['ConstituentID'], # TODO: Artista
+                          lt.getElement(result,i)['DateAcquired'],
+                          lt.getElement(result,i)['Medium'],
+                          lt.getElement(result,i)['Dimensions']])
+        answ._max_width = {'Título':40,'Artista(s)':40,'Fecha':20,'Medio':40,
+                            'Dimensiones':40}
+        print(answ)
 
     elif (inputs>3) and (inputs<8):
         print("Este requerimiento aún no se ha implementado.")
