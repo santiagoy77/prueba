365bcae4e773626440827a1f1c85f7e5c4965de2
Ehimar Vargas <e.vargasm@uniandes.edu.co>
2022-04-05 22:38:02 -0500

    Prueba en rama 1

--- App/model.py
+++ App/model.py
@@ -6,7 +6,7 @@
  * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
  *
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is freesdfasdfsf software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
