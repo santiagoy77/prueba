bab26cf55061c939a262292badf012ba709e81c7
s.yepesg@unianes.edu.co <s.yepesg@uniandes.edu.co>
2024-06-18 15:07:07 -0500

    ddd

--- App/model.py
+++ App/model.py
@@ -1,73 +1,189 @@
-﻿import config as cf
+﻿"""
+ * Copyright 2020, Departamento de sistemas y Computación,
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contribuciones:
+ *
+ * Dario Correal - Version inicial
+ """
+
+
+import config as cf
 from DISClib.ADT import list as lt
+from DISClib.ADT import stack as st
+from DISClib.ADT import queue as qu
+from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Algorithms.Sorting import insertionsort as ins
+from DISClib.Algorithms.Sorting import selectionsort as se
 from DISClib.Algorithms.Sorting import mergesort as merg
-import csv
+from DISClib.Algorithms.Sorting import quicksort as quk
+assert cf
+
+"""
+Se define la estructura de un catálogo de videos. El catálogo tendrá
+dos listas, una para los videos, otra para las categorias de los mismos.
+"""
+
+# Construccion de modelos
+
 
 def new_data_structs():
     """
     Inicializa las estructuras de datos del modelo. Las crea de
     manera vacía para posteriormente almacenar la información.
     """
-    data_structs = {
-        "data": lt.newList("SINGLE_LINKED", compare),
-        "years": {}
-    }
-    return data_structs
+    #TODO: Inicializar las estructuras de datos
+    pass
 
-def add_data(data_structs, row):
+
+# Funciones para agregar informacion al modelo
+
+def add_data(data_structs, data):
     """
     Función para agregar nuevos elementos a la lista
     """
-    lt.addLast(data_structs["data"], row)
-    year = row["Año"]
-    if year not in data_structs["years"]:
-        data_structs["years"][year] = lt.newList("SINGLE_LINKED", compare)
-    lt.addLast(data_structs["years"][year], row)
+    #TODO: Crear la función para agregar elementos a una lista
+    pass
+
+
+# Funciones para creacion de datos
+
+def new_data(id, info):
+    """
+    Crea una nueva estructura para modelar los datos
+    """
+    #TODO: Crear la función para estructurar los datos
+    pass
+
+
+# Funciones de consulta
 
 def get_data(data_structs, id):
     """
     Retorna un dato a partir de su ID
     """
-    data = data_structs["data"]
-    for elem in lt.iterator(data):
-        if elem["id"] == id:
-            return elem
-    return None
+    #TODO: Crear la función para obtener un dato de una lista
+    pass
+
 
 def data_size(data_structs):
     """
     Retorna el tamaño de la lista de datos
     """
-    return lt.size(data_structs["data"])
+    #TODO: Crear la función para obtener el tamaño de una lista
+    pass
 
-def get_years(data_structs):
-    """
-    Retorna los años disponibles en los datos
-    """
-    return list(data_structs["years"].keys())
 
-def get_year_data(data_structs, year):
+def req_1(data_structs):
     """
-    Retorna los datos de un año específico
+    Función que soluciona el requerimiento 1
     """
-    if year in data_structs["years"]:
-        year_data = data_structs["years"][year]
-        sorted_year_data = merg.sort(year_data, sort_criteria)
-        first_3 = lt.subList(sorted_year_data, 1, 3)
-        last_3 = lt.subList(sorted_year_data, lt.size(sorted_year_data) - 2, 3)
-        return first_3 + last_3
-    return None
+    # TODO: Realizar el requerimiento 1
+    pass
 
-def sort_criteria(data_1, data_2):
+
+def req_2(data_structs):
     """
-    Criterio de ordenamiento por Año y Código actividad económica
+    Función que soluciona el requerimiento 2
     """
-    if data_1["Año"] != data_2["Año"]:
-        return data_1["Año"] < data_2["Año"]
-    return data_1["Código actividad económica"] < data_2["Código actividad económica"]
+    # TODO: Realizar el requerimiento 2
+    pass
+
+
+def req_3(data_structs):
+    """
+    Función que soluciona el requerimiento 3
+    """
+    # TODO: Realizar el requerimiento 3
+    pass
+
+
+def req_4(data_structs):
+    """
+    Función que soluciona el requerimiento 4
+    """
+    # TODO: Realizar el requerimiento 4
+    pass
+
+
+def req_5(data_structs):
+    """
+    Función que soluciona el requerimiento 5
+    """
+    # TODO: Realizar el requerimiento 5
+    pass
+
+
+def req_6(data_structs):
+    """
+    Función que soluciona el requerimiento 6
+    """
+    # TODO: Realizar el requerimiento 6
+    pass
+
+
+def req_7(data_structs):
+    """
+    Función que soluciona el requerimiento 7
+    """
+    # TODO: Realizar el requerimiento 7
+    pass
+
+
+def req_8(data_structs):
+    """
+    Función que soluciona el requerimiento 8
+    """
+    # TODO: Realizar el requerimiento 8
+    pass
+
+
+# Funciones utilizadas para comparar elementos dentro de una lista
 
 def compare(data_1, data_2):
     """
     Función encargada de comparar dos datos
     """
-    return data_1 == data_2
+    #TODO: Crear función comparadora de la lista
+    pass
+
+# Funciones de ordenamiento
+
+
+def sort_criteria(data_1, data_2):
+    """sortCriteria criterio de ordenamiento para las funciones de ordenamiento
+
+    Args:
+        data1 (_type_): _description_
+        data2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    #TODO: Crear función comparadora para ordenar
+    pass
+
+
+def sort(data_structs):
+    """
+    Función encargada de ordenar la lista con los datos
+    """
+    #TODO: Crear función de ordenamiento
+    pass
--- App/view.py
+++ App/view.py
@@ -1,50 +1,143 @@
-﻿import config as cf
+﻿"""
+ * Copyright 2020, Departamento de sistemas y Computación, Universidad
+ * de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+import config as cf
 import sys
 import controller
+from DISClib.ADT import list as lt
+from DISClib.ADT import stack as st
+from DISClib.ADT import queue as qu
+assert cf
 from tabulate import tabulate
 import traceback
 
+"""
+La vista se encarga de la interacción con el usuario
+Presenta el menu de opciones y por cada seleccion
+se hace la solicitud al controlador para ejecutar la
+operación solicitada
+"""
+
+
 def new_controller():
     """
-    Se crea una instancia del controlador
+        Se crea una instancia del controlador
     """
-    return controller.new_controller()
+    #TODO: Llamar la función del controlador donde se crean las estructuras de datos
+    pass
+
 
 def print_menu():
     print("Bienvenido")
     print("1- Cargar información")
     print("2- Ejecutar Requerimiento 1")
+    print("3- Ejecutar Requerimiento 2")
+    print("4- Ejecutar Requerimiento 3")
+    print("5- Ejecutar Requerimiento 4")
+    print("6- Ejecutar Requerimiento 5")
+    print("7- Ejecutar Requerimiento 6")
+    print("8- Ejecutar Requerimiento 7")
+    print("9- Ejecutar Requerimiento 8")
     print("0- Salir")
+
 
 def load_data(control):
     """
     Carga los datos
     """
-    try:
-        control = controller.new_controller()
-        filename = 'Salida_agregados_renta_juridicos_AG_large.csv'
-        controller.load_data(control, filename)
-        total_rows = controller.data_size(control)
-        print(f"Total de filas cargadas: {total_rows}")
-        for year in controller.get_years(control):
-            print(f"Año: {year}")
-            data = controller.get_year_data(control, year)
-            if data:
-                print(tabulate(data, headers=["Año", "Código actividad económica", "Nombre actividad económica", "Código sector económico", "Nombre sector económico", "Código subsector económico", "Nombre subsector económico", "Total ingresos netos", "Total costos y gastos", "Total saldo a pagar", "Total saldo a favor"], tablefmt="grid"))
-    except Exception as e:
-        print("Error al cargar los datos:", e)
-        traceback.print_exc()
+    #TODO: Realizar la carga de datos
+    pass
+
+
+def print_data(control, id):
+    """
+        Función que imprime un dato dado su ID
+    """
+    #TODO: Realizar la función para imprimir un elemento
+    pass
 
 def print_req_1(control):
     """
-    Función que imprime la solución del Requerimiento 1 en consola
+        Función que imprime la solución del Requerimiento 1 en consola
     """
-    try:
-        result = controller.req_1(control)
-        print(tabulate(result, headers=["Año", "Código actividad económica", "Nombre actividad económica", "Código sector económico", "Nombre sector económico", "Código subsector económico", "Nombre subsector económico", "Total ingresos netos", "Total costos y gastos", "Total saldo a pagar", "Total saldo a favor"], tablefmt="grid"))
-    except Exception as e:
-        print("Error al ejecutar el requerimiento 1:", e)
-        traceback.print_exc()
+    # TODO: Imprimir el resultado del requerimiento 1
+    pass
+
+
+def print_req_2(control):
+    """
+        Función que imprime la solución del Requerimiento 2 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 2
+    pass
+
+
+def print_req_3(control):
+    """
+        Función que imprime la solución del Requerimiento 3 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 3
+    pass
+
+
+def print_req_4(control):
+    """
+        Función que imprime la solución del Requerimiento 4 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 4
+    pass
+
+
+def print_req_5(control):
+    """
+        Función que imprime la solución del Requerimiento 5 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 5
+    pass
+
+
+def print_req_6(control):
+    """
+        Función que imprime la solución del Requerimiento 6 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 6
+    pass
+
+
+def print_req_7(control):
+    """
+        Función que imprime la solución del Requerimiento 7 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 7
+    pass
+
+
+def print_req_8(control):
+    """
+        Función que imprime la solución del Requerimiento 8 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 8
+    pass
+
 
 # Se crea el controlador asociado a la vista
 control = new_controller()
@@ -61,12 +154,34 @@ if __name__ == "__main__":
         inputs = input('Seleccione una opción para continuar\n')
         if int(inputs) == 1:
             print("Cargando información de los archivos ....\n")
-            load_data(control)
+            data = load_data(control)
         elif int(inputs) == 2:
             print_req_1(control)
+
+        elif int(inputs) == 3:
+            print_req_2(control)
+
+        elif int(inputs) == 4:
+            print_req_3(control)
+
+        elif int(inputs) == 5:
+            print_req_4(control)
+
+        elif int(inputs) == 6:
+            print_req_5(control)
+
+        elif int(inputs) == 7:
+            print_req_6(control)
+
+        elif int(inputs) == 8:
+            print_req_7(control)
+
+        elif int(inputs) == 9:
+            print_req_8(control)
+
         elif int(inputs) == 0:
             working = False
-            print("\nGracias por utilizar el programa")
+            print("\nGracias por utilizar el programa") 
         else:
             print("Opción errónea, vuelva a elegir.\n")
     sys.exit(0)
