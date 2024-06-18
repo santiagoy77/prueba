56abec5963a721f6e0b157a57a347eaf873e6f3f
s.yepesg@unianes.edu.co <s.yepesg@uniandes.edu.co>
2024-06-18 15:03:48 -0500

    TYUIOP

--- App/controller.py
+++ App/controller.py
@@ -1,149 +1,48 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
-import config as cf
+﻿import config as cf
 import model
-import time
 import csv
-
-
-"""
-El controlador se encarga de mediar entre la vista y el modelo.
-"""
-
 
 def new_controller():
     """
     Crea una instancia del modelo
     """
-    #TODO: Llamar la función del modelo que crea las estructuras de datos
-    pass
-
-
-# Funciones para la carga de datos
+    control = {
+        "model": model.new_data_structs()
+    }
+    return control
 
 def load_data(control, filename):
     """
     Carga los datos del reto
     """
-    # TODO: Realizar la carga de datos
-    pass
+    data_structs = control["model"]
+    filepath = cf.data_dir + filename
+    with open(filepath, encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            model.add_data(data_structs, row)
+    return data_structs
 
-
-# Funciones de ordenamiento
-
-def sort(control):
+def data_size(control):
     """
-    Ordena los datos del modelo
+    Retorna el tamaño de la lista de datos
     """
-    #TODO: Llamar la función del modelo para ordenar los datos
-    pass
+    return model.data_size(control["model"])
 
-
-# Funciones de consulta sobre el catálogo
-
-def get_data(control, id):
+def get_years(control):
     """
-    Retorna un dato por su ID.
+    Retorna los años disponibles en los datos
     """
-    #TODO: Llamar la función del modelo para obtener un dato
-    pass
+    return model.get_years(control["model"])
 
+def get_year_data(control, year):
+    """
+    Retorna los datos de un año específico
+    """
+    return model.get_year_data(control["model"], year)
 
 def req_1(control):
     """
     Retorna el resultado del requerimiento 1
     """
-    # TODO: Modificar el requerimiento 1
-    pass
-
-
-def req_2(control):
-    """
-    Retorna el resultado del requerimiento 2
-    """
-    # TODO: Modificar el requerimiento 2
-    pass
-
-
-def req_3(control):
-    """
-    Retorna el resultado del requerimiento 3
-    """
-    # TODO: Modificar el requerimiento 3
-    pass
-
-
-def req_4(control):
-    """
-    Retorna el resultado del requerimiento 4
-    """
-    # TODO: Modificar el requerimiento 4
-    pass
-
-
-def req_5(control):
-    """
-    Retorna el resultado del requerimiento 5
-    """
-    # TODO: Modificar el requerimiento 5
-    pass
-
-def req_6(control):
-    """
-    Retorna el resultado del requerimiento 6
-    """
-    # TODO: Modificar el requerimiento 6
-    pass
-
-
-def req_7(control):
-    """
-    Retorna el resultado del requerimiento 7
-    """
-    # TODO: Modificar el requerimiento 7
-    pass
-
-
-def req_8(control):
-    """
-    Retorna el resultado del requerimiento 8
-    """
-    # TODO: Modificar el requerimiento 8
-    pass
-
-
-# Funciones para medir tiempos de ejecucion
-
-def get_time():
-    """
-    devuelve el instante tiempo de procesamiento en milisegundos
-    """
-    return float(time.perf_counter()*1000)
-
-
-def delta_time(start, end):
-    """
-    devuelve la diferencia entre tiempos de procesamiento muestreados
-    """
-    elapsed = float(end - start)
-    return elapsed
+    return model.req_1(control["model"])
--- App/model.py
+++ App/model.py
@@ -1,189 +1,73 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contribuciones:
- *
- * Dario Correal - Version inicial
- """
-
-
-import config as cf
+﻿import config as cf
 from DISClib.ADT import list as lt
-from DISClib.ADT import stack as st
-from DISClib.ADT import queue as qu
-from DISClib.Algorithms.Sorting import shellsort as sa
-from DISClib.Algorithms.Sorting import insertionsort as ins
-from DISClib.Algorithms.Sorting import selectionsort as se
 from DISClib.Algorithms.Sorting import mergesort as merg
-from DISClib.Algorithms.Sorting import quicksort as quk
-assert cf
-
-"""
-Se define la estructura de un catálogo de videos. El catálogo tendrá
-dos listas, una para los videos, otra para las categorias de los mismos.
-"""
-
-# Construccion de modelos
-
+import csv
 
 def new_data_structs():
     """
     Inicializa las estructuras de datos del modelo. Las crea de
     manera vacía para posteriormente almacenar la información.
     """
-    #TODO: Inicializar las estructuras de datos
-    pass
+    data_structs = {
+        "data": lt.newList("SINGLE_LINKED", compare),
+        "years": {}
+    }
+    return data_structs
 
-
-# Funciones para agregar informacion al modelo
-
-def add_data(data_structs, data):
+def add_data(data_structs, row):
     """
     Función para agregar nuevos elementos a la lista
     """
-    #TODO: Crear la función para agregar elementos a una lista
-    pass
-
-
-# Funciones para creacion de datos
-
-def new_data(id, info):
-    """
-    Crea una nueva estructura para modelar los datos
-    """
-    #TODO: Crear la función para estructurar los datos
-    pass
-
-
-# Funciones de consulta
+    lt.addLast(data_structs["data"], row)
+    year = row["Año"]
+    if year not in data_structs["years"]:
+        data_structs["years"][year] = lt.newList("SINGLE_LINKED", compare)
+    lt.addLast(data_structs["years"][year], row)
 
 def get_data(data_structs, id):
     """
     Retorna un dato a partir de su ID
     """
-    #TODO: Crear la función para obtener un dato de una lista
-    pass
-
+    data = data_structs["data"]
+    for elem in lt.iterator(data):
+        if elem["id"] == id:
+            return elem
+    return None
 
 def data_size(data_structs):
     """
     Retorna el tamaño de la lista de datos
     """
-    #TODO: Crear la función para obtener el tamaño de una lista
-    pass
+    return lt.size(data_structs["data"])
 
-
-def req_1(data_structs):
+def get_years(data_structs):
     """
-    Función que soluciona el requerimiento 1
+    Retorna los años disponibles en los datos
     """
-    # TODO: Realizar el requerimiento 1
-    pass
+    return list(data_structs["years"].keys())
 
-
-def req_2(data_structs):
+def get_year_data(data_structs, year):
     """
-    Función que soluciona el requerimiento 2
+    Retorna los datos de un año específico
     """
-    # TODO: Realizar el requerimiento 2
-    pass
+    if year in data_structs["years"]:
+        year_data = data_structs["years"][year]
+        sorted_year_data = merg.sort(year_data, sort_criteria)
+        first_3 = lt.subList(sorted_year_data, 1, 3)
+        last_3 = lt.subList(sorted_year_data, lt.size(sorted_year_data) - 2, 3)
+        return first_3 + last_3
+    return None
 
-
-def req_3(data_structs):
+def sort_criteria(data_1, data_2):
     """
-    Función que soluciona el requerimiento 3
+    Criterio de ordenamiento por Año y Código actividad económica
     """
-    # TODO: Realizar el requerimiento 3
-    pass
-
-
-def req_4(data_structs):
-    """
-    Función que soluciona el requerimiento 4
-    """
-    # TODO: Realizar el requerimiento 4
-    pass
-
-
-def req_5(data_structs):
-    """
-    Función que soluciona el requerimiento 5
-    """
-    # TODO: Realizar el requerimiento 5
-    pass
-
-
-def req_6(data_structs):
-    """
-    Función que soluciona el requerimiento 6
-    """
-    # TODO: Realizar el requerimiento 6
-    pass
-
-
-def req_7(data_structs):
-    """
-    Función que soluciona el requerimiento 7
-    """
-    # TODO: Realizar el requerimiento 7
-    pass
-
-
-def req_8(data_structs):
-    """
-    Función que soluciona el requerimiento 8
-    """
-    # TODO: Realizar el requerimiento 8
-    pass
-
-
-# Funciones utilizadas para comparar elementos dentro de una lista
+    if data_1["Año"] != data_2["Año"]:
+        return data_1["Año"] < data_2["Año"]
+    return data_1["Código actividad económica"] < data_2["Código actividad económica"]
 
 def compare(data_1, data_2):
     """
     Función encargada de comparar dos datos
     """
-    #TODO: Crear función comparadora de la lista
-    pass
-
-# Funciones de ordenamiento
-
-
-def sort_criteria(data_1, data_2):
-    """sortCriteria criterio de ordenamiento para las funciones de ordenamiento
-
-    Args:
-        data1 (_type_): _description_
-        data2 (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    #TODO: Crear función comparadora para ordenar
-    pass
-
-
-def sort(data_structs):
-    """
-    Función encargada de ordenar la lista con los datos
-    """
-    #TODO: Crear función de ordenamiento
-    pass
+    return data_1 == data_2
